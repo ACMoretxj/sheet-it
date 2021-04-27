d7bacf421ec158a9d683d0ec73d664d456115c44
Ferris Tien <9417900+ACMoretxj@users.noreply.github.com>
2021-04-27 14:44:50 +0800

    Updated file naming mechanism for ConversionTemplateGeneration project (#1)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var csv = require('csv-parser');
 var fse = require('fs-extra');
 var path = require('path');
 var followRedirects = require('follow-redirects');
+const moment = require('moment');
 
 followRedirects.maxRedirects = 10;
 var https = followRedirects.https;
@@ -9,48 +10,56 @@ var https = followRedirects.https;
 var sheets = JSON.parse(fse.readFileSync('sheets.json'));
 
 function editLinkToCsvLink(url) {
-    var newUrl = url.replace('edit#gid=0', 'export?format=csv');
-    newUrl = newUrl.replace('edit?usp=sharing','export?format=csv');
+    const editPostfixes = [ 'edit#gid=0', 'edit?usp=sharing' ];
+    let newUrl = url;
+    for (const editPostfix of editPostfixes) {
+        newUrl = newUrl.replace(editPostfix, 'export?format=csv');
+    }
     return newUrl;
 }
 
 function rowFileName(rowData, headers) {
-    const exclusions = ['Link', 'Comment', 'Status', 'Owner'];
-    var fileNameComponents = [];
-    for (var i = 0; i < headers.length; i++) {
-        if (!exclusions.includes(headers[i])) {
-            if (rowData[headers[i]] && rowData[headers[i]].length > 0) {
-                fileNameComponents.push(rowData[headers[i]]);
-            }
-        }
+    let fileName = rowData[headers[0]];
+    const exclusions = [ 'HL7 Data Type - FHIR R4: ', 'HL7 Segment - FHIR R4: ', 'HL7 Message - FHIR R4: ' ];
+    const replacements = [ '/', ':', ' ' ];
+    for (let exclusion of exclusions) {
+        fileName = fileName.replace(exclusion, '');
     }
-    return fileNameComponents.join('-').split(/[/:]/).join('_').split(' ').join('');
+    for (let replacement of replacements) {
+        fileName = fileName.replace(replacement, '_');
+    }
+    return fileName;
 }
 
-sheets["sheets"].forEach(s => {
-    var folder = path.join('out', s.name.replace(' ', ''));
-    fse.ensureDirSync(folder);
-    https.get(s.url, function (response) {
-        var headers;
-        response
-            .pipe(csv())
-            .on('headers', (h) => {
-                headers = h;
-            })
-            .on('data', (data) => {
-                try {
-                    https.get(editLinkToCsvLink(data.Link), function(sheetResponse) {
-                        if (sheetResponse.statusCode == 200) {
-                            var file = fse.createWriteStream(path.join(folder, rowFileName(data, headers) + ".csv"));
-                            sheetResponse.pipe(file);
-                        } else {
-                            console.log('Error accessing file for ' + rowFileName(data, headers) + ', ' + editLinkToCsvLink(data.Link));
-                        }
-                    });
-                }
-                catch(err) {
-                    console.log(`Error : [${err}] for ${rowFileName(data, headers)},  ${editLinkToCsvLink(data.Link)}`);
-                }
-            });
+(function main () {
+    const rootFolder = path.join('./out', moment().format('YYYY-MM-DD_HH-mm-SS'));
+
+    sheets["sheets"].forEach(s => {
+        const destFolder = path.join(rootFolder, s.name);
+        fse.ensureDirSync(destFolder);
+    
+        https.get(s.url, function (response) {
+            var headers;
+            response.pipe(csv())
+                .on('headers', (h) => {
+                    headers = h;
+                })
+                .on('data', (data) => {
+                    try {
+                        https.get(editLinkToCsvLink(data.Link), function(sheetResponse) {
+                            if (sheetResponse.statusCode == 200) {
+                                const filePath = path.join(destFolder, rowFileName(data, headers) + ".csv");
+                                var file = fse.createWriteStream(filePath);
+                                sheetResponse.pipe(file);
+                            } else {
+                                console.log('Error accessing file for ' + rowFileName(data, headers) + ', ' + editLinkToCsvLink(data.Link));
+                            }
+                        });
+                    }
+                    catch(err) {
+                        console.log(`Error : [${err}] for ${rowFileName(data, headers)},  ${editLinkToCsvLink(data.Link)}`);
+                    }
+                });
+        });
     });
-});
+})();
